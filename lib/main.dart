e8cb6c461250dd11735cd542213ea191cd4bb0d0
ririssilvia <ririssilvia1@gmail.com>
2021-02-19 15:47:51 +0700

    Widget Container

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,60 +6,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Contoh Date Picker',
-      home: MyHomePage(title: 'Contoh Date Picker'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-// Variable/State untuk mengambil tanggal
-  DateTime selectedDate = DateTime.now();
-// Initial SelectDate FLutter
-  Future<Null> _selectDate(BuildContext context) async {
-// Initial DateTime FIinal Picked
-    final DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: selectedDate,
-        firstDate: DateTime(2015, 8),
-        lastDate: DateTime(2101));
-    if (picked != null && picked != selectedDate)
-      setState(() {
-        selectedDate = picked;
-      });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Text("${selectedDate.toLocal()}".split(' ')[0]),
-            SizedBox(
-              height: 20.0,
-            ),
-            RaisedButton(
-              onPressed: () => {
-                _selectDate(context),
-                print(selectedDate.day + selectedDate.month + selectedDate.year)
-              },
-              child: Text('Pilih Tanggal'),
-            ),
-          ],
-        ),
-      ),
+      home: Scaffold(
+          appBar: AppBar(title: Text("Contoh Button")),
+          body: Container(
+              child: RaisedButton(
+            textColor: Colors.white,
+            onPressed: () {},
+            color: Colors.red[900],
+            child: Text("Button", style: TextStyle(fontSize: 20)),
+          ))),
     );
   }
 }
