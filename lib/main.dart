b5a8bf8e64b6906b36cc5eedd3897120850b73ff
ririssilvia <ririssilvia1@gmail.com>
2021-02-19 15:28:17 +0700

    WidgwrCapertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Image(
-        image: NetworkImage(
-            'https://www.wildlifeaid.org.uk/wp-content/uploads/2019/11/BarnOwl-1619x1080.jpg'),
+      home: Container(
+        margin: EdgeInsets.only(top: 30),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            AppBar(title: Text('Contoh Cupertino')),
+            CupertinoButton(
+              child: Text("Contoh button"),
+              onPressed: () {},
+            ),
+            CupertinoActivityIndicator(),
+          ],
+        ),
       ),
     );
   }
